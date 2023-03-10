4972bdf4ca68d95f2e27ce479688978b91d8fe9b
FoKit <fokit@qq.com>
2023-03-10 13:48:50 +0800

    Update jd1_api.js

--- scripts/jd1_api.js
+++ scripts/jd1_api.js
@@ -56,7 +56,6 @@ $.ql = {
       url: `${$.ql_url}/${this.type}/envs?searchValue=${searchValue}`,
       headers: this.headers,
     };
-    console.log('test:', opt)
     return $.http.get(opt).then((response) => JSON.parse(response.body));
   },
   configs(fileName = 'config.sh') {
@@ -65,7 +64,6 @@ $.ql = {
       url: `${$.ql_url}/${this.type}/configs/${fileName}?t=${Date.now()}`,
       headers: this.headers,
     };
-    console.log('test:', opt)
     return $.http.get(opt).then((response) => JSON.parse(response.body));
   },
 };
