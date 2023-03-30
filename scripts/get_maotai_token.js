505491c5e88e0f23661661ee77eb4d4535d188da
FoKit <fokit@qq.com>
2023-03-30 11:20:54 +0800

    updata

--- scripts/get_maotai_token.js
+++ scripts/get_maotai_token.js
@@ -20,7 +20,6 @@ hostname = %APPEND% app.moutai519.com.cn
 */
 
 const $ = new Env('i茅台Token');
-$.MT_VERSION_KEY = 'MT_VERSION';
 $.MT_TOKENS_KEY = 'MT_TOKENS';
 $.MT_TOKENS = $.getdata($.MT_TOKENS_KEY) || '';
 
@@ -30,26 +29,38 @@ $.MT_TOKENS = $.getdata($.MT_TOKENS_KEY) || '';
   }
 
   function GetCookie() {
-    if ($request && $request.headers && ($request.headers['MT-Token'] && $request.headers['MT-Device-ID'] || $request.headers['mt-token'] && $request.headers['mt-device-id'])) {
-      debug($request.headers);
-      let new_MT_Token = $request.headers['MT-Token'] || $request.headers['mt-token'];
-      let new_Device_ID = $request.headers['MT-Device-ID'] || $request.headers['mt-device-id'];
-      let old_MT_Token = $.MT_TOKENS.split(',') ? $.MT_TOKENS.split(',')[1] : '';
-      if (old_MT_Token !== new_MT_Token) {
-        $.setdata(new_Device_ID + ',' + new_MT_Token, $.MT_TOKENS_KEY);
-        $.msg($.name, `🎉 Token获取成功。`, `${new_Device_ID + ',' + new_MT_Token}`);
-      } else {
-        $.log(`无需更新 MT-Token:\n${new_Device_ID + ',' + new_MT_Token}\n`);
+    if ($request && $request.headers) {
+      log($request.headers);
+      if (($request.headers['MT-Token'] && $request.headers['MT-Device-ID']) || ($request.headers['mt-token'] && $request.headers['mt-device-id'])) {
+        let new_MT_Token = $request.headers['MT-Token'] || $request.headers['mt-token'];
+        let new_Device_ID = $request.headers['MT-Device-ID'] || $request.headers['mt-device-id'];
+        let old_MT_Token = $.MT_TOKENS.split(',') ? $.MT_TOKENS.split(',')[1] : '';
+        if (old_MT_Token !== new_MT_Token) {
+          $.setdata(new_Device_ID + ',' + new_MT_Token, $.MT_TOKENS_KEY);
+          $.msg($.name, `🎉 Token获取成功`, `${new_Device_ID + ',' + new_MT_Token}`);
+        } else {
+          $.log(`无需更新 MT-Token:\n${new_Device_ID + ',' + new_MT_Token}\n`);
+        }
       }
       if ($request.headers['MT-APP-Version'] || $request.headers['mt-app-version']) {
         $.MT_VERSION = $request.headers['MT-APP-Version'] || $request.headers['mt-app-version'];
-        $.setdata($.MT_VERSION, $.MT_VERSION_KEY);
+        $.setdata($.MT_VERSION, `MT_VERSION`);
         $.log(`🎉 MT_VERSION 写入成功:\n${$.MT_VERSION}\n`);
+      }
+      if ($request.headers['User-Agent'] || $request.headers['user-agent']) {
+        $.MT_USERAGENT = $request.headers['User-Agent'] || $request.headers['user-agent'];
+        $.setdata($.MT_USERAGENT, `MT_USERAGENT`);
+        $.log(`🎉 MT_USERAGENT 写入成功:\n${$.MT_USERAGENT}\n`);
+      }
+      if ($request.headers['MT-R'] || $request.headers['mt-r']) {
+        $.MT_R = $request.headers['MT-R'] || $request.headers['mt-r'];
+        $.setdata($.MT_R, `MT_R`);
+        $.log(`🎉 MT_R 写入成功:\n${$.MT_R}\n`);
       }
     }
   }
 
-  function debug(text) {
+  function log(text) {
     if ($.getdata('is_debug') === 'true') {
       if (typeof text == "string") {
         console.log(text);
