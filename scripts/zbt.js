0069add1222539f4a11debf6192c3325538ce846
FoKit <fokit@qq.com>
2023-07-17 10:42:02 +0800

    Update zbt.js

--- scripts/zbt.js
+++ scripts/zbt.js
@@ -5,9 +5,24 @@
 
 [rewrite_local]
 ^https:\/\/api\.gzgame99\.cn\/zc\/user\/setGameData url script-request-body https://raw.githubusercontent.com/FoKit/Scripts/main/scripts/zbt.js
+^https:\/\/api\.gzgame99\.cn\/zc\/user\/getGameData url script-response-body https://raw.githubusercontent.com/FoKit/Scripts/main/scripts/zbt.js
 
 [MITM]
 hostname = api.gzgame99.cn
 */
 
-$done({ body: $request.body.replace(/"curStrength":\d+,/g, `"curStrength":99999`).replace(/"1":\d+,/g, `"1":99999,`) });
+
+let str = '';
+
+if ($request.url.indexOf("setGameData")) {
+  str = $request.body;
+  console.log('上传存档');
+} else if ($request.url.indexOf("getGameData")) {
+  str = $response.body;
+  console.log('下载存档');
+}
+
+str = str.replace(/"curStrength":\d+,/g, `"curStrength":99999,`).replace(/"1":\d+,/g, `"1":99999,`);
+console.log('修改成功');
+
+$done({ body: str });
