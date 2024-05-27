0c886ee3a202d3e3a8c23b143915ae441c2313aa
FoKit <fokit@qq.com>
2024-05-27 22:24:05 +0800

    fix (get_jd_wskey.js) 新版 App 获取 wskey

--- scripts/get_jd_wskey.js
+++ scripts/get_jd_wskey.js
@@ -1,48 +1,39 @@
 /*
 脚本名称：京东 WSKEY
-更新时间：2024-05-11
-使用方法：划掉后台重新打开 京东APP 即可自动抓取 WSKEY
+更新时间：2024-05-27
+使用方法：划掉后台重新打开 京东APP 即可自动抓取 WSKEY。抓完 WSKEY 不能在京东 app 点退出登录（会导致 WSKEY 失效），切换账号的正确姿势是先断网（飞行模式）再点击退出登录，划掉后台重新打开 APP 再登录新的账号。
 注意事项：脚本抓取的 WSKEY 默认自动提交到服务器（自动上车），可通过 BoxJs 设置关闭自动提交功能。
 重写订阅：https://raw.githubusercontent.com/FoKit/Scripts/main/rewrite/get_jd_wskey.sgmodule
 BoxJs订阅：https://raw.githubusercontent.com/FoKit/Scripts/main/boxjs/fokit.boxjs.json
 
 ------------------ Surge 配置 ------------------
 
-[Rule]
-URL-REGEX,^https:\/\/wlogin\.m\.jd\.com\/applogin_v2$,REJECT
-
-URL-REGEX,^https:\/\/wlmonitor\.m\.jd\.com\/login_report$,REJECT
-
 [Script]
-京东 WSKEY = type=http-request,pattern=https:\/\/api\.m\.jd\.com\/client\.action\?functionId=xview2Config,requires-body=0,max-size=0,binary-body-mode=0,timeout=30,script-path=https://raw.githubusercontent.com/FoKit/Scripts/main/scripts/get_jd_wskey.js,script-update-interval=0
+京东 WSKEY = type=http-request,pattern=https:\/\/blackhole\.m\.jd\.com\/getinfo,requires-body=0,max-size=0,binary-body-mode=0,timeout=30,script-path=https://raw.githubusercontent.com/FoKit/Scripts/main/scripts/get_jd_wskey.js,script-update-interval=0
 
 京东 PIN = type=http-request,pattern=https:\/\/perf\.m\.jd\.com\/app_monitor\/v2\/getRule,requires-body=0,max-size=0,binary-body-mode=0,timeout=30,script-path=https://raw.githubusercontent.com/FoKit/Scripts/main/scripts/get_jd_wskey.js,script-update-interval=0
 
 [MITM]
-hostname = %APPEND% api.m.jd.com, perf.m.jd.com, wlogin.m.jd.com, wlmonitor.m.jd.com
+hostname = %APPEND% blackhole.m.jd.com, perf.m.jd.com
 
 ------------------- Loon 配置 -------------------
 
 [MITM]
-hostname = api.m.jd.com, perf.m.jd.com
+hostname = blackhole.m.jd.com, perf.m.jd.com
 
 [Script]
-http-request https:\/\/api\.m\.jd\.com\/client\.action\?functionId=xview2Config tag=京东 WSKEY,script-path=https://raw.githubusercontent.com/FoKit/Scripts/main/scripts/get_jd_wskey.js,requires-body=0
+http-request https:\/\/blackhole\.m\.jd\.com\/getinfo tag=京东 WSKEY,script-path=https://raw.githubusercontent.com/FoKit/Scripts/main/scripts/get_jd_wskey.js,requires-body=0
 
 http-request https:\/\/perf\.m\.jd\.com\/app_monitor\/v2\/getRule tag=京东 PIN,script-path=https://raw.githubusercontent.com/FoKit/Scripts/main/scripts/get_jd_wskey.js,requires-body=0
 
 --------------- Quantumult X 配置 ---------------
 
 [MITM]
-hostname = api.m.jd.com, perf.m.jd.com, wlogin.m.jd.com, wlmonitor.m.jd.com
+hostname = blackhole.m.jd.com, perf.m.jd.com
 
 [rewrite_local]
 
-^https:\/\/wlogin\.m\.jd\.com\/applogin_v2$ url reject
-
-^https:\/\/wlmonitor\.m\.jd\.com\/login_report$ url reject
-
-https:\/\/api\.m\.jd\.com\/client\.action\?functionId=xview2Config url script-request-header https://raw.githubusercontent.com/FoKit/Scripts/main/scripts/get_jd_wskey.js
+https:\/\/blackhole\.m\.jd\.com\/getinfo url script-request-header https://raw.githubusercontent.com/FoKit/Scripts/main/scripts/get_jd_wskey.js
 
 https:\/\/perf\.m\.jd\.com\/app_monitor\/v2\/getRule url script-request-header https://raw.githubusercontent.com/FoKit/Scripts/main/scripts/get_jd_wskey.js
 
